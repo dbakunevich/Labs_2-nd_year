a4db7c5bf3ac80929cfd9a26e1b6e86600a999db
dmitry <d.bakunevich@g.nsu.ru>
2020-10-30 21:54:49 +0700

    1-st commit

---
+++ LaboratoryWorks/Lab_2/cmake-build-debug/CMakeFiles/lab_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_2.dir/graphics/visualizer.cpp.o"
+  "CMakeFiles/lab_2.dir/graphics/window.cpp.o"
+  "CMakeFiles/lab_2.dir/main/main.cpp.o"
+  "lab_2"
+  "lab_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
